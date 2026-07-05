4283b6b4759cb42da4e5bd893b53cfb2d4bac9ca
ChristianRossKane <christian.kane@hotmail.com>
2026-07-05 16:55:13 -0500

    Adding more rudimentary and skeleton code.

--- LoadBalancer.h
+++ LoadBalancer.h
@@ -1,7 +1,66 @@
 ///////////////////////////////////////////////////////////////////////////////
 // LoadBalancer Class : Header Definition
+// "Load balancers are available commercially, but in this project, you will 
+//  build one that has:
+//  ● Requests - generates random IP addresses, both in and out, and random 
+//    times for each request to be processed.
+//      o IP in
+//      o IP out
+//      o Time (integer)
+//      o Job Type (Character, p (processing), or s (streaming))
+//  ● Webserver(s)
+//      o Takes requests from the Load Balancer
+//      o Processes the requests
+//      o Asks for another
+//  ● Load Balancer
+//      o Queue of requests
+//      o Keeps track of time."
 ///////////////////////////////////////////////////////////////////////////////
 
+///////////////////////////////////////////////////////////////////////////////
+//  ● Requests - generates random IP addresses, both in and out, and random 
+//    times for each request to be processed.
+//      o IP in
+//      o IP out
+//      o Time (integer)
+//      o Job Type (Character, p (processing), or s (streaming))
+///////////////////////////////////////////////////////////////////////////////
+struct Request
+{
+    // ...
+};
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//  ● Requests - generates random IP addresses, both in and out, and random 
+//    times for each request to be processed.
+//      o IP in
+//      o IP out
+//      o Time (integer)
+//      o Job Type (Character, p (processing), or s (streaming))
+///////////////////////////////////////////////////////////////////////////////
+class WebServer
+{
+public:
+
+    // ...
+
+private:
+
+    // ...
+
+};
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//  ● Load Balancer
+//      o Queue of requests
+//      o Keeps track of time.
+///////////////////////////////////////////////////////////////////////////////
 class LoadBalancer
 {
 public:
--- main.cpp
+++ main.cpp
@@ -3,10 +3,48 @@
 
 int main()
 {
+    ///////////////////////////////////////////////////////////////////////////
+    // "The main program should set up the number of servers (user input), the 
+    // time you want to run the load balancer (user input), and generate a full
+    // queue (usually servers * 100)."
+    // 
+    // ...
+    //
+    // You will need to add new requests at random times to simulate new 
+    // requests after the initial full queue you set up.
+    ///////////////////////////////////////////////////////////////////////////
 
-    std::cout << "LoadBalancer Main Test." << std::endl;
+    //
+    // Intro messages:
+    std::cout << "****************************************************************************************************" << std::endl;
+    std::cout << "\nWelcome to Christian Kane's CSCE 412 Cloud Computing Load Balancer Simulation!" << std::endl;
+    std::cout << "****************************************************************************************************" << std::endl;
 
+    //
+    // Get the number of servers.
+    int nServers = 10;
+    std::cout << "\nEnter a number of servers to simulate requests between: ";
+    std::cin >> nServers;
+
+    //
+    // Get the total simulated clock cycles:
+    int nClockCycles = 10000;
+    std::cout << "\nEnter a number of simulated clock cycles: ";
+    std::cin >> nClockCycles;
+
+    //
+    // Initialize locals for the simulation.
     LoadBalancer lb;
+
+    //
+    // Create a log of 10 servers running for 10000 clock cycles
+    for (int i = 1; i <= nClockCycles; ++i) {
+        if (i % 1000 == 0)
+        std::cout << "Cycle " << i << std::endl;
+
+        //
+        // ...
+    }
 
     return 0;           
 }
